b47769b32abaec59de2c987c8df243b57f341701
Ayesha qureshi <qureshiayesha660@gmail.com>
2024-08-25 11:25:16 +0530

    json

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
